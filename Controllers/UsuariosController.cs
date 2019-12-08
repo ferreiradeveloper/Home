32860d63cd2655e97b1ab43c3688baa80dafa753
Francisco <peter@peterdeveloper.com>
2019-12-08 22:08:13 +0100

    Add project files.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Usuarios.Controllers
+{
+    [Route("/Nombre")]
+    public class UsuariosController : Controller
+    {
+        //[HttpGet]
+        [Route("/Nombre/Frank")]
+        public IActionResult Index(string data, int age = 51)
+        {
+            String datos = data+ " " + age;
+            return View("Index",datos);
+        }
+
+        public IActionResult Index()
+        {            
+            return View();
+        }
+    }
+}
