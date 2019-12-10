39153b6f0b0af81bdcb88ac6bfc040418013dec9
Francisco <peter@peterdeveloper.com>
2019-12-10 01:24:38 +0100

    video 12, Controlador de codigos UseStatusCodePages

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Usuarios.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace Usuarios
 {
@@ -50,6 +51,15 @@ namespace Usuarios
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //app.UseStatusCodePages("text/plain","Pagina de Codigos de Estado, codigo de estado: {0}");
+            app.UseStatusCodePages(async context => {
+                await context.HttpContext.Response.WriteAsync(
+                    "Pagina de Codigos de Estado, codigo de estado:" +
+                    context.HttpContext.Response.StatusCode
+                    );
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
