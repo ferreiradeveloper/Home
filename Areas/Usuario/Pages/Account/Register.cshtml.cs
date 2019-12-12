1a4fdb38060bdfee42a30e231f34928cc8644689
Francisco <peter@peterdeveloper.com>
2019-12-13 00:00:41 +0100

    Hasta el video 29 registrando usuarios

--- Areas/Usuario/Pages/Account/Register.cshtml.cs
+++ Areas/Usuario/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,6 +12,11 @@ namespace Usuarios.Areas.Usuario.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private UserManager<IdentityUser> _userManager;
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public void OnGet(string data)
         {
            
@@ -39,23 +45,47 @@ namespace Usuarios.Areas.Usuario.Pages.Account
             [Compare("Password", ErrorMessage = "Error: Contraceña y Confirmacion no coinciden")]
             public string ConfirmarPassword { get; set; }
 
-            [Required]
-            public string ErrorMessage { get; set; }
-
-            
+            //[Required]
+            public string ErrorMessage { get; set; }            
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
             if (ModelState.IsValid)
             {
-
+                var userList = _userManager.Users.Where(u => u.Email.Equals(Input.Email)).ToList();
+                if (userList.Count.Equals(0))
+                {
+                    var user = new IdentityUser
+                    {
+                        UserName = Input.Email,
+                        Email = Input.Email
+                    };
+                    var result = await _userManager.CreateAsync(user, Input.Password);
+                    if (result.Succeeded)
+                    {
+                        return Page();
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            Input = new InputModel
+                            {
+                                ErrorMessage = item.Description,
+                            };
+                        }
+                        return Page();
+                    }
+                }
+                else
+                {
+                    Input = new InputModel
+                    {
+                        ErrorMessage = $"El {Input.Email} ya esta registrado",
+                    };
+                }
             }
-            else
-            {
-                ModelState.AddModelError("Input.Email","Error en el server");
-            }
-            var data = Input;
             return Page();
         }
         
