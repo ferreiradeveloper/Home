b582a2d68cfda6ac61f18a181a491fcd03dfb588
Francisco Ferreira <45833609+ferreiradeveloper@users.noreply.github.com>
2019-12-12 17:14:08 +0100

    video 22, capturando datos de la pagina

--- Areas/Usuario/Pages/Account/Register.cshtml.cs
+++ Areas/Usuario/Pages/Account/Register.cshtml.cs
@@ -25,6 +25,24 @@ namespace Usuarios.Areas.Usuario.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+
+            [Required]
+            [Display(Name = "Contraseña")]
+            [DataType(DataType.Password)]
+            [StringLength(100, ErrorMessage = "El numero de caracteres de {0} debe ser al menos {2}", MinimumLength = 8)]
+            public string Password { get; set; }
+
+            
+            [Required]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "Error: Contraceña y Confirmacion no coinciden")]
+            public string ConfirmarPassword { get; set; }
+        }
+
+        public IActionResult OnPost()
+        {
+            var data = Input;
+            return Page();
         }
         
     }
